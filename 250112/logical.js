ee2dd622d964ee93b434860dbbab6cb9db16e7c5
leesongyi <song0478@naver.com>
2025-01-12 11:42:16 +0900

    250112공부

---
+++ 250112/logical.js
@@ -0,0 +1,26 @@
+// 논리연산자 Logical operator
+// && 그리고
+// || 또는
+// ! 부정(단항연산자에서 온것)
+// !! 불리언값으로 변환 (단항연산자 응용버전)
+
+let num = 21;
+if (num >= 0 || num < 20) {
+  console.log("굿");
+}
+
+if (num != 8) {
+  console.log("힝");
+}
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log(!"text");
